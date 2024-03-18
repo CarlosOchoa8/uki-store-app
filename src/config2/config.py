d189abe5c4b9f1d4beeda445d91e2b381c714572
carlos-ochoa8 <carlosochoa@tae.capital>
2024-03-18 10:41:43 -0600

    Refactor: DATABASE_URL comments where deleted, doesnt alter the functionlaity of project

--- src/config2/config.py
+++ src/config2/config.py
@@ -6,8 +6,6 @@ from config2.statics import static
 class Config:
     SECRET_KEY = os.getenv("SECRET_KEY")
     DEBUG = True
-    # SQLALCHEMY_DATABASE_URI = "mysql://root:Password1!@localhost/ukituki_store_db"
     SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL")
-    # SQLALCHEMY_DATABASE_URI = "sqlite:///ukituki_store.db"
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     UPLOAD_FOLDER = static
