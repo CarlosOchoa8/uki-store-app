f7c4d7f568c0b12ff59450eda98190ff83dcdc45
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-05-26 19:36:37 -0600

    fix: user update forms and profile route.

--- src/forms/users_forms.py
+++ src/forms/users_forms.py
@@ -1,9 +1,12 @@
+from collections.abc import Sequence
+from typing import Any, Mapping
 from flask_wtf import FlaskForm
-from wtforms import StringField, IntegerField, PasswordField, EmailField, SubmitField
-from wtforms.validators import DataRequired, Length, Email, ValidationError
+from wtforms import EmailField, PasswordField, StringField, SubmitField
+from wtforms.validators import DataRequired, Email, Length, Optional
 
 
 class UserCreateForm(FlaskForm):
+    """Form for request data for User creation"""
     first_name = StringField("Nombre(s)", validators=[
         DataRequired(),
         Length(min=3, max=50, message="Tu(s) nombre(s) debe(n) tener almenos 3 letras.")]
@@ -19,14 +22,24 @@ class UserCreateForm(FlaskForm):
 
 
 class UserUpdateForm(FlaskForm):
-    first_name = StringField("Nombre(s)", validators=[
-        DataRequired(),
-        Length(min=3, max=50, message="Tu(s) nombre(s) debe(n) tener almenos 3 letras.")]
-                             )
+    """Form for request data for User Updating"""
+    first_name = StringField("Nombre(s)",
+                             validators=[
+                                 Optional(""),
+                                 Length(min=3, max=50, message="Tu(s) nombre(s) debe(n) tener almenos 3 letras.")]
+                                 )
     last_name = StringField("Apellido(s)",
-                            validators=[DataRequired("Tu(s) apellido(s) debe(n) tener almenos 3 letras."),
-                                        Length(min=3, max=50)]
-                            )
-    email = EmailField("Email", validators=[DataRequired(), Email()])
-    phone_number = StringField("Teléfono", validators=[DataRequired(), Length(max=10, min=10)])
+                            validators=[Optional(),
+                                        Length(min= 3,
+                                               max= 50,
+                                               message= "Tu(s) nombre(s) debe(n) tener almenos 3 letras.")],
+                                               )
+    email = EmailField("Email", validators=[Optional(),
+                                            Email(message="Dirección email inválida.")])
+    phone_number = StringField("Teléfono", validators=[Optional(),
+                                                       Length(
+                                                           max=10,
+                                                           min=10,
+                                                           message="El número debe contener 10 dígitos.")]
+                                                           )
     submit = SubmitField("Actualizar información.")
