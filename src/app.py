dd21b49933ede6034e8d39399120fb481d6dca0c
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-06-10 20:25:11 -0600

    add: 404 not found route, html and style
    FIX: Removed comments in app, referenced for correct index on base.html

--- src/app.py
+++ src/app.py
@@ -1,18 +1,18 @@
 """Init for Flask app module."""
-from smtplib import SMTPException
 from typing import Any
 
-from flask import Flask, flash, redirect, render_template, request, url_for
-from flask_mail import Mail, Message
+from flask import Flask, jsonify, render_template
 
-import models
 from config2 import config
 from database.db import db
-from forms.contact_forms import ContactUsForm
-from routes import (address_blueprint, auth_blueprint, main_blueprint,
-                    products_blueprint, users_blueprint)
+from routes import (auth_blueprint, main_blueprint, products_blueprint,
+                    users_blueprint)
 
 app = Flask(__name__)
+@app.errorhandler(404)
+def not_found(e):
+    return render_template("404.html")
+
 class PrefixMiddleware:
     """Middleware for url path name"""
     def __init__(self, app, prefix) -> None:
@@ -27,7 +27,7 @@ class PrefixMiddleware:
         elif environ["PATH_INFO"] == "/":
             environ["PATH_INFO"] = "/index.html"
         return self.app(environ, start_response)
-    
+
 app.wsgi_app = PrefixMiddleware(app.wsgi_app, prefix="/ukitukistore")
 
 app_config = config.AppConfig()
@@ -45,77 +45,8 @@ with app.app_context():
 
 @app.route("/")
 def index():
-    return "{'message': 'OK'}"
-    # print(app.blueprints)
-    # products = models.Product.query.all()
-    # return render_template("index.html", products=products)
-
-
-# @app.route("/about_us", methods=["GET"])
-# def about_us():
-#     """
-#     Render template for about us html
-#     """
-#     return render_template("about_us.html")
-
-
-# @app.route("/faq", methods=["GET"])
-# def frecuent_questions_answers():
-#     """
-#     Render template for faq html
-#     """
-#     return render_template("faq.html")
-
-# @app.route("/contact_us", methods=["GET", "POST"])
-# def contact_us():
-#     """
-#     Render template for faq html
-#     """
-#     form = ContactUsForm()
-#     if request.method == "POST" and form.validate_on_submit():
-#         # TODO
-#         form_in = form.data
-#         name = form_in["name"]
-#         last_name = form_in["last_name"]
-#         email = form_in["email"]
-#         message = form_in["message"]
-
-#         mail = Mail(app)
-#         support_mail = Message(subject="Soporte al Cliente - Ukituki Store",
-#                       recipients=["fidelito6080@gmail.com"])
-#         support_mail.body = message
-#         support_mail.html = render_template(
-#             "support_email.html",
-#             customer_name=name,
-#             customer_last_name=last_name,
-#             customer_email=email,
-#             customer_message=message
-#         )
-
-#         customer_mail = Message(subject="Soporte al Cliente - Ukituki Store",
-#                                 recipients=[email])
-#         customer_mail.body = message
-#         customer_mail.html = render_template(
-#             "reply_email.html",
-#             customer_name=name,
-#             customer_last_name=last_name,
-#             customer_email=email,
-#             customer_message=message
-#         )
-
-#         try:
-#             mail.send(support_mail)
-#             mail.send(customer_mail)
-#         except SMTPException as exc:
-#             flash(
-#                 message=f"Error al enviar el correo: {exc}"
-#             )
-#         except Exception as exc:
-#             flash(
-#                 message=f"Error al enviar el correo: {exc}"
-#             )
-#         return redirect(url_for("contact_us"))
-#     return render_template("contact_us.html", form=form)
+    """Root method to check app availability."""
+    return jsonify({"Service": "Ok"})
 
 
 if __name__ == "__main__":
