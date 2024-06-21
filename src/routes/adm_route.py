f7e1f3daf404b69400d2d6f57d4e0f2df9030d1f
CarlosOchoa8 <ochoa.carlos8@outlook.com>
2024-06-20 20:45:49 -0600

    reset hard comm be7bdcaab0ad4

--- src/routes/adm_route.py
+++ src/routes/adm_route.py
@@ -1,4 +1,6 @@
-from flask import Blueprint, Response, render_template
+from flask import Blueprint, Response, render_template, request
+
+from forms import ProductCreateForm
 
 panel_blueprint = Blueprint("panel", __name__)
 
@@ -7,6 +9,16 @@ panel_blueprint = Blueprint("panel", __name__)
 def panel() -> Response | str:
     """Render admin panel."""
     return render_template("adm_base.html")
+
+
+@panel_blueprint.route("/product/create", methods=["GET", "POST"])
+def create_product():
+    """Add new product at store."""
+    print("Entro")
+    form = ProductCreateForm()
+    if request.method == "POST" and form.validate_on_submit():
+        print("asdasd")
+    return render_template("adm/products/create.html", form=form)
 
 
 panel_route = panel_blueprint
